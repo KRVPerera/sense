912c43b361351550733a7a7397c519ff3208a3b4
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2023-11-08 16:21:18 +0200

    fix: remove extra flag

--- tools/monitor.sh
+++ tools/monitor.sh
@@ -1,2 +1,3 @@
-
-iotlab-profile addm3 -n group12 -voltage -current -power -period 8244 -avg 4
+#!/usr/bin/env bash
+#iotlab-profile addm3 -p battery -n g12_m3 -voltage -current -power -period 8244 -avg 4
+iotlab-profile addm3 -n g12_m3 -voltage -current -power -period 8244 -avg 4

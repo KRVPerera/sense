b269fc5328bd93f070b887a6337a1bf949d6261c
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2023-11-08 16:21:53 +0200

    fix: add the consumption help link

--- tools/monitor.sh
+++ tools/monitor.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+# https://www.iot-lab.info/docs/tools/consumption-monitoring/
 #iotlab-profile addm3 -p battery -n g12_m3 -voltage -current -power -period 8244 -avg 4
 iotlab-profile addm3 -n g12_m3 -voltage -current -power -period 8244 -avg 4

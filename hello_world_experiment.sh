4198734df8eb4a66fce16632fbe25d8ef304bb15
KRVPerera <rukshan.viduranga@gmail.com>
2023-11-09 03:12:25 +0200

    copy elf file

--- hello_world_experiment.sh
+++ hello_world_experiment.sh
@@ -4,11 +4,12 @@
 if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
     source /opt/riot.source
 else
-    echo "The environment variable IOT_LAB_FRONTEND_FQDN is not set."
+    echo "[ERROR] The environment variable IOT_LAB_FRONTEND_FQDN is not set."
 fi
 
 ARCH=nrf52840dk
 
 make BOARD=${ARCH} -C tutorials_riotos/hello-world
+echo tutorials_riotos/hello-world/bin/${ARCH}/hello-world.elf
 cp tutorials_riotos/hello-world/bin/${ARCH}/hello-world.elf ~/shared/
 

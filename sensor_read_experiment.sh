eea836b417977d4c91029b16cb2942d1f27aaae5
KRVPerera <rukshan.viduranga@gmail.com>
2023-11-10 21:37:35 +0200

    fix: senor m3 path

--- sensor_read_experiment.sh
+++ sensor_read_experiment.sh
@@ -13,8 +13,8 @@ ARCH=iotlab-m3
 # sensor nodes 20,21,22, 359, 361, 362
 
 make BOARD=${ARCH} -C tutorials_riotos/sensor-m3
-echo tutorials_riotos/hello-world/bin/${ARCH}/sensor.elf
-cp tutorials_riotos/hello-world/bin/${ARCH}/sensor.elf ~/shared/
+echo tutorials_riotos/sensor-m3/bin/${ARCH}/sensor.elf
+cp tutorials_riotos/sensor-m3/bin/${ARCH}/sensor.elf ~/shared/
 
 iotlab-profile del -n group12
 iotlab-profile addm3 -n group12 -voltage -current -power -period 8244 -avg 4

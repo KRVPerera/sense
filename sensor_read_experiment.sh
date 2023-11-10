e1dd61f8015ac9f97b7a7e6f661fd70a2e2950e9
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2023-11-10 21:34:19 +0200

    fix:change firmware file

--- sensor_read_experiment.sh
+++ sensor_read_experiment.sh
@@ -19,5 +19,5 @@ cp tutorials_riotos/hello-world/bin/${ARCH}/sensor.elf ~/shared/
 iotlab-profile del -n group12
 iotlab-profile addm3 -n group12 -voltage -current -power -period 8244 -avg 4
 ## submitting a job in iot test bed with the firmware it self
-iotlab-experiment submit -n senor-read-g12 -d 3 -l grenoble,m3,359-361,~/shared/hello-world.elf,group12
+iotlab-experiment submit -n senor-read-g12 -d 3 -l grenoble,m3,359-361,~/shared/sensor.elf,group12
 iotlab-experiment wait --timeout 30 --cancel-on-timeout

b6725eaa941e2551cf6e5fbe8295fe784d4ae7ef
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2023-11-10 21:40:28 +0200

    fix: typo

--- sensor_read_experiment.sh
+++ sensor_read_experiment.sh
@@ -13,11 +13,11 @@ ARCH=iotlab-m3
 # sensor nodes 20,21,22, 359, 361, 362
 
 make BOARD=${ARCH} -C tutorials_riotos/sensor-m3
-echo tutorials_riotos/sensor-m3/bin/${ARCH}/sensor.elf
-cp tutorials_riotos/sensor-m3/bin/${ARCH}/sensor.elf ~/shared/
+echo tutorials_riotos/sensor-m3/bin/${ARCH}/sensors.elf
+cp tutorials_riotos/sensor-m3/bin/${ARCH}/sensors.elf ~/shared/
 
 iotlab-profile del -n group12
 iotlab-profile addm3 -n group12 -voltage -current -power -period 8244 -avg 4
 ## submitting a job in iot test bed with the firmware it self
-iotlab-experiment submit -n senor-read-g12 -d 3 -l grenoble,m3,359-361,~/shared/sensor.elf,group12
+iotlab-experiment submit -n senor-read-g12 -d 3 -l grenoble,m3,359-361,~/shared/sensors.elf,group12
 iotlab-experiment wait --timeout 30 --cancel-on-timeout

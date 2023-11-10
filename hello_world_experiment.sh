2e434e812ab746439bacc9e608568b58b26cf126
KRVPerera <rukshan.viduranga@gmail.com>
2023-11-10 21:48:10 +0200

    feat: now the run script provide a shell also

--- hello_world_experiment.sh
+++ hello_world_experiment.sh
@@ -19,6 +19,8 @@ cp tutorials_riotos/hello-world/bin/${ARCH}/hello-world.elf ~/shared/
 iotlab-profile del -n group12
 iotlab-profile addm3 -n group12 -voltage -current -power -period 8244 -avg 4
 ## submitting a job in iot test bed with the firmware it self
-iotlab-experiment submit -n hello_gp_12 -d 3 -l grenoble,m3,359-361,~/shared/hello-world.elf,group12
+iotlab-experiment submit -n hello_gp_12 -d 3 -l grenoble,m3,36161--361,~/shared/hello-world.elf,group12
 iotlab-experiment wait --timeout 30 --cancel-on-timeout
+iotlab-experiment --jmespath="items[*].network_address | sort(@)" get --nodes
+make IOTLAB_NODE=auto term
 

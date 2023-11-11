f5fd5c4a4283cb10b1eb248835b67569c832800b
Ubuntu <waasnipun@gmail.com>
2023-11-11 21:21:02 +0000

    fix: refactoring the code

--- src/server/coap_server.py
+++ src/server/coap_server.py
@@ -19,22 +19,6 @@ class TimeResource(resource.ObservableResource):
                 strftime("%Y-%m-%d %H:%M").encode('ascii')
         return aiocoap.Message(payload=payload)
 
-class WhoAmI(resource.Resource):
-    async def render_get(self, request):
-        text = ["Used protocol: %s." % request.remote.scheme]
-
-        text.append("Request came from %s." % request.remote.hostinfo)
-        text.append("The server address used %s." % request.remote.hostinfo_local)
-
-        claims = list(request.remote.authenticated_claims)
-        if claims:
-            text.append("Authenticated claims of the client: %s." % ", ".join(repr(c) for c in claims))
-        else:
-            text.append("No claims authenticated.")
-
-        return aiocoap.Message(content_format=0,
-                payload="\n".join(text).encode('utf8'))
-
 class temperature(resource.Resource):
     async def render_post(self, request):
         payload = json.loads(request.payload.decode('utf8'))
@@ -48,7 +32,6 @@ async def main():
     root.add_resource(['.well-known', 'core'],
             resource.WKCResource(root.get_resources_as_linkheader))
     root.add_resource(['time'], TimeResource())
-    root.add_resource(['whoami'], WhoAmI())
     root.add_resource(['temp'], temperature())
 
     await aiocoap.Context.create_server_context(root, bind=('0.0.0.0', 5683))

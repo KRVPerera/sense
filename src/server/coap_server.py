f303f6394a79881ff62f0bbb8d9a87df0538d656
Ubuntu <waasnipun@gmail.com>
2023-11-11 13:40:02 +0000

    Containerizing the coap server

--- src/server/coap_server.py
+++ src/server/coap_server.py
@@ -1,29 +1,26 @@
 import asyncio
 from aiocoap import Context, Message
-import boto3
-import json
 
 # CoAP Server
 async def coap_server(request):
     # Process CoAP message
     # Send the message to SQS
     message = request.payload.decode('utf-8')
-    print(message)
     send_to_sqs(message)
     return Message(payload=b"Message received successfully")
 
 # SQS Sender
 def send_to_sqs(message):
-    sqs = boto3.resource('sqs')
-    queue = sqs.get_queue_by_name(QueueName='sense_iot')
-
-    response = queue.send_message(MessageBody=message)
-    print(f"Message sent to SQS: {response['MessageId']}")
+    pass
 
 # Run CoAP server
 async def main():
-    context = await Context.create_server_context(coap_server)
-    await context
+    context = await Context.create_server_context(coap_server, bind=('0.0.0.0', 5683))
+    print("CoAP server started on 0.0.0.0:5683")
+    try:
+        await asyncio.Future()  # Keep the event loop running
+    finally:
+        context.shutdown()
 
 if __name__ == "__main__":
     asyncio.run(main())

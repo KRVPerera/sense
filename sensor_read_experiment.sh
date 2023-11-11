80d68e9c799e9af47c587112a46aa2aea3f1d907
KRVPerera <rukshan.viduranga@gmail.com>
2023-11-11 08:52:31 +0200

    feat: add testbed env code

--- sensor_read_experiment.sh
+++ sensor_read_experiment.sh
@@ -2,9 +2,9 @@
 #
 
 if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
-    source /opt/riot.source
+  source /opt/riot.source
 else
-    echo "[ERROR] The environment variable IOT_LAB_FRONTEND_FQDN is not set."
+  echo "[ERROR] The environment variable IOT_LAB_FRONTEND_FQDN is not set."
 fi
 
 #ARCH=nrf52840dk # not available widely in the test bed
@@ -14,13 +14,16 @@ ARCH=iotlab-m3
 
 make BOARD=${ARCH} -C tutorials_riotos/sensor-m3
 echo tutorials_riotos/sensor-m3/bin/${ARCH}/sensors.elf
-cp tutorials_riotos/sensor-m3/bin/${ARCH}/sensors.elf ~/shared/
 
-iotlab-profile del -n group12
-iotlab-profile addm3 -n group12 -voltage -current -power -period 8244 -avg 4
-## submitting a job in iot test bed with the firmware it self
-iotlab-experiment submit -n senor-read-g12 -d 3 -l grenoble,m3,356,~/shared/sensors.elf,group12
-iotlab-experiment wait --timeout 30 --cancel-on-timeout
+if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
+  cp tutorials_riotos/sensor-m3/bin/${ARCH}/sensors.elf ~/shared/
 
-iotlab-experiment --jmespath="items[*].network_address | sort(@)" get --nodes
-make IOTLAB_NODE=auto -C tutorials_riotos/sensor-m3 term
+  iotlab-profile del -n group12
+  iotlab-profile addm3 -n group12 -voltage -current -power -period 8244 -avg 4
+  ## submitting a job in iot test bed with the firmware it self
+  iotlab-experiment submit -n senor-read-g12 -d 3 -l grenoble,m3,356,~/shared/sensors.elf,group12
+  iotlab-experiment wait --timeout 30 --cancel-on-timeout
+
+  iotlab-experiment --jmespath="items[*].network_address | sort(@)" get --nodes
+  make IOTLAB_NODE=auto -C tutorials_riotos/sensor-m3 term
+fi

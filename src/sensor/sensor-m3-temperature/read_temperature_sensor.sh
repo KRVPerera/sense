9ec80f7996278c085359dfb311adcf7e7d0d19a1
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2023-11-11 14:59:09 +0200

    feat: read only temperatur sensor data

---
+++ src/sensor/sensor-m3-temperature/read_temperature_sensor.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#
+
+if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
+  source /opt/riot.source
+else
+  echo "[ERROR] The environment variable IOT_LAB_FRONTEND_FQDN is not set."
+fi
+
+#ARCH=nrf52840dk # not available widely in the test bed
+ARCH=iotlab-m3
+
+# sensor nodes 20,21,22, 359, 361, 362
+
+make BOARD=${ARCH}
+echo bin/${ARCH}/sensors.elf
+
+if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
+  cp bin/${ARCH}/sensors.elf ~/shared/
+
+  iotlab-profile del -n group12
+  iotlab-profile addm3 -n group12 -voltage -current -power -period 8244 -avg 4
+
+  ## submitting a job in iot test bed with the firmware it self
+  iotlab-experiment submit -n senor-read-g12 -d 3 -l grenoble,m3,356,~/shared/sensors.elf,group12
+  iotlab-experiment wait --timeout 30 --cancel-on-timeout
+
+  iotlab-experiment --jmespath="items[*].network_address | sort(@)" get --nodes
+  make IOTLAB_NODE=auto term

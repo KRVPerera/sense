ab21e8399bd534b2832347e88408ee81e5d2b01a
KRVPerera <rukshan.viduranga@gmail.com>
2023-11-11 14:25:11 +0100

    read: read sensor date each 5 seconds

--- src/sensor/sensor-m3-temperature/read_temperature_sensor.sh
+++ src/sensor/sensor-m3-temperature/read_temperature_sensor.sh
@@ -13,17 +13,18 @@ ARCH=iotlab-m3
 # sensor nodes 20,21,22, 359, 361, 362
 
 make BOARD=${ARCH}
-echo bin/${ARCH}/sensors.elf
+echo bin/${ARCH}/sensor_temperature.elf
 
 if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
-  cp bin/${ARCH}/sensors.elf ~/shared/
+  cp bin/${ARCH}/sensor_temperature.elf ~/shared/
 
   iotlab-profile del -n group12
   iotlab-profile addm3 -n group12 -voltage -current -power -period 8244 -avg 4
 
   ## submitting a job in iot test bed with the firmware it self
-  iotlab-experiment submit -n senor-read-g12 -d 3 -l grenoble,m3,356,~/shared/sensors.elf,group12
+  iotlab-experiment submit -n senor-temp-read-g12 -d 3 -l grenoble,m3,356,~/shared/sensor_temperature.elf,group12
   iotlab-experiment wait --timeout 30 --cancel-on-timeout
 
   iotlab-experiment --jmespath="items[*].network_address | sort(@)" get --nodes
   make IOTLAB_NODE=auto term
+fi

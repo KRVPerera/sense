a80857bd071f1b702e51129baa9bdc33e6c10ff4
Ubuntu <waasnipun@gmail.com>
2023-11-11 21:15:51 +0000

    fix: basic coap server running in ec2 completed

--- src/server/coap_server.py
+++ src/server/coap_server.py
@@ -1,5 +1,6 @@
 import datetime
 import logging
+import json
 
 import asyncio
 
@@ -7,8 +8,12 @@ import aiocoap.resource as resource
 from aiocoap.numbers.contentformat import ContentFormat
 import aiocoap
 
-class TimeResource(resource.ObservableResource):
+# logging setup
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("coap-server").setLevel(logging.DEBUG)
 
+
+class TimeResource(resource.ObservableResource):
     async def render_get(self, request):
         payload = datetime.datetime.now().\
                 strftime("%Y-%m-%d %H:%M").encode('ascii')
@@ -30,10 +35,11 @@ class WhoAmI(resource.Resource):
         return aiocoap.Message(content_format=0,
                 payload="\n".join(text).encode('utf8'))
 
-# logging setup
-
-logging.basicConfig(level=logging.INFO)
-logging.getLogger("coap-server").setLevel(logging.DEBUG)
+class temperature(resource.Resource):
+    async def render_post(self, request):
+        payload = json.loads(request.payload.decode('utf8'))
+        return aiocoap.Message(content_format=0,
+                payload=json.dumps({"status": payload['input']}).encode('utf8'))
 
 async def main():
     # Resource tree creation
@@ -43,6 +49,7 @@ async def main():
             resource.WKCResource(root.get_resources_as_linkheader))
     root.add_resource(['time'], TimeResource())
     root.add_resource(['whoami'], WhoAmI())
+    root.add_resource(['temp'], temperature())
 
     await aiocoap.Context.create_server_context(root, bind=('0.0.0.0', 5683))
 

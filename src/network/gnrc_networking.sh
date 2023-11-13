dda1e37de8c55cae028e8999f89d391b73a0a027
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2023-11-13 09:37:58 +0100

    feat: networking node

--- src/network/gnrc_networking.sh
+++ src/network/gnrc_networking.sh
@@ -12,16 +12,15 @@ ARCH=iotlab-m3
 
 # sensor nodes 20,21,22, 359, 361, 362
 
-make BOARD=${ARCH} -C gnrc_networking.elf
+make BOARD=${ARCH} -C gnrc_networking
 echo gnrc_networking/bin/${ARCH}/gnrc_networking.elf
 
 if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
   cp gnrc_networking/bin/${ARCH}/gnrc_networking.elf ~/shared/
 
   ## submitting a job in iot test bed with the firmware it self
-  iotlab-experiment submit -n gnrc_networking_gp12 -temp-read-g12 -d 10 -l grenoble,m3,361,~/shared/gnrc_networking.elf
+  iotlab-experiment submit -n gnrc_networking_gp12 -d 5 -l grenoble,m3,361,~/shared/gnrc_networking.elf
   iotlab-experiment wait --timeout 30 --cancel-on-timeout
 
   iotlab-experiment --jmespath="items[*].network_address | sort(@)" get --nodes
-  iotlab-experiment stop
 fi

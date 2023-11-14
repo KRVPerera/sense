3ba7211e91d73555f6260c4f698e9a057ea8cd65
Ubuntu <waasnipun@gmail.com>
2023-11-14 15:22:22 +0000

    fix: ipv6 issue in coap fixed

--- src/server/coap_client.py
+++ src/server/coap_client.py
@@ -1,6 +1,6 @@
 import logging
 import asyncio
-
+import json
 from aiocoap import *
 import aiocoap
 
@@ -9,7 +9,8 @@ logging.basicConfig(level=logging.INFO)
 async def main():
     protocol = await Context.create_client_context()
 
-    request = Message(code=aiocoap.GET, uri='coap://172.17.0.2:5683/time')
+    payload = json.dumps({'temp': [1,5,3,5], 'pressure': [3894, 399, 90.55, "oh yey ojaye"]}).encode("utf-8")
+    request = Message(code=aiocoap.POST, payload=payload, uri='coap://[2a05:d016:1bb:3e00:2fbe:1fb4:63f9:eb4b]:5683/temp')
 
     try:
         response = await protocol.request(request).response

699a55af83ab93f8f6a4994894f453cfc072cd8f
KRVPerera <rukshan.viduranga@gmail.com>
2023-11-18 21:35:26 +0200

    fix: coap local test ok

---
+++ src/csc_test_server/simple_coap_message_client.py
@@ -0,0 +1,20 @@
+import asyncio
+from aiocoap import *
+
+async def main():
+    protocol = await Context.create_client_context()
+
+    payload = b"This is a test message"
+    request = Message(code=POST, payload=payload, uri='coap://localhost/message')
+    
+    try:
+        response = await protocol.request(request).response
+    except Exception as e:
+        print('Failed to send message:')
+        print(e)
+    else:
+        print('Result: %s\n%r'%(response.code, response.payload))
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
+
--- src/csc_test_server/simple_coap_server.py
+++ src/csc_test_server/simple_coap_server.py
@@ -2,12 +2,17 @@ from aiocoap import *
 import aiocoap.resource as resource
 import asyncio
 import logging
+from aiocoap import CHANGED, POST, Message, Context
 
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("CoAPServer")
 
+class MessageResource(resource.Resource):
+    async def render_post(self, request):
+        logger.info(f"Received POST request with payload: {request.payload.decode()}")
+        return Message(code=CHANGED, payload=request.payload)
 
 class BasicResource(resource.Resource):
     async def render_get(self, request):
@@ -17,9 +22,12 @@ class BasicResource(resource.Resource):
 def main():
     root = resource.Site()
     root.add_resource(['hello'], BasicResource())
+    root.add_resource(['message'], MessageResource())
     asyncio.Task(Context.create_server_context(root))
     logger.info("CoAP server started")
     asyncio.get_event_loop().run_forever()
 
 if __name__ == "__main__":
    main()
+
+

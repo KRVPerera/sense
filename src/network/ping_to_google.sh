432ee67013cad2968b13b925b357b50d99d15d5b
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2023-11-18 16:25:51 +0100

    style: get ready for node testing

--- src/network/ping_to_google.sh
+++ src/network/ping_to_google.sh
@@ -9,34 +9,50 @@ fi
 
 #ARCH=nrf52840dk # not available widely in the test bed
 ARCH=iotlab-m3
+NODE_1=28 # border router
+NODE_2=26
 
 # sensor nodes 20,21,22, 359, 361, 362
 
+echo "Build border router"
 make ETHOS_BAUDRATE=500000 DEFAULT_CHANNEL=23 BOARD=${ARCH} -C gnrc_border_router
+echo "Build normal network node"
 make ETHOS_BAUDRATE=500000 DEFAULT_CHANNEL=23 BOARD=${ARCH} -C gnrc_networking
 
 if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
+  echo "Copy firmware files to shared"
   cp gnrc_border_router/bin/${ARCH}/gnrc_border_router.elf ~/shared/
   cp gnrc_networking/bin/${ARCH}/gnrc_networking.elf ~/shared/
 
   # submit border router job and save job id
-  border_router_job_json=$(iotlab-experiment submit -n n2n-border-router-gp12 -d 30 -l grenoble,m3,28,~/shared/gnrc_border_router.elf)
+  echo "Submit job to node 28"
+  border_router_job_json=$(iotlab-experiment submit -n n2n-border-router-gp12 -d 30 -l grenoble,m3,$NODE_1,~/shared/gnrc_border_router.elf)
   border_router_job_id=$(echo $border_router_job_json | jq '.id')
   
   # wait for border router to start
   iotlab-experiment wait --timeout 30 --cancel-on-timeout -i $border_router_job_id --state Running
 
   # submit network router node job and save job id
-  n_json=$(iotlab-experiment submit -n n2n-networking-node-gp12 -d 30 -l grenoble,m3,26,~/shared/gnrc_networking.elf)
+  echo "Submit job to node 26"
+  n_json=$(iotlab-experiment submit -n n2n-networking-node-gp12 -d 30 -l grenoble,m3,$NODE_2,~/shared/gnrc_networking.elf)
   n_node_job_id=$(echo $n_json | jq '.id')
+
+  # create a file to stop the experiments
+  echo "Creating 'stopper.sh' script"
+  touch stopper.sh
+  echo "iotlab-experiment stop -i $n_node_job_id" > stopper.sh
+  echo "iotlab-experiment stop -i $border_router_job_id" >> stopper.sh
 
   # wait for network node to start
   iotlab-experiment wait --timeout 30 --cancel-on-timeout -i $n_node_job_id --state Running
 
+  # https://www.iot-lab.info/legacy/tutorials/understand-ipv6-subnetting-on-the-fit-iot-lab-testbed/index.html
   # create tap interface in background
-  sudo ethos_uhcpd.py m3-28 tap7 2001:660:5307:30ff::1/64
+  echo "Create tap interface 7"
+  sudo ethos_uhcpd.py m3-$NODE_1 tap7 2001:660:5307:3107::1/64 &
 
   # sleep sometime to allow interface to be created
+  echo "I am sleeping for few seconds..."
   sleep 5
 
   # connecting to intermediate router
@@ -44,7 +60,10 @@ if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
   # ifconfig
   # and also ping google
   # ping 2001:4860:4860::8888 or 2001:4860:4860::8844
-  nc m3-26 20000
+  echo ""
+  echo "You are connected to m3-26 node"
+  echo "try to ping to google : ping 2001:4860:4860::8888"
+  nc m3-$NODE_2 20000
 
   # stop network node job (experiment)
   iotlab-experiment stop -i $n_node_job_id

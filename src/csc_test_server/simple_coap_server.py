ba17d43c9596836489fe9a8be1c8ceaf3ac2cbfa
KRVPerera <rukshan.viduranga@gmail.com>
2023-11-18 21:20:14 +0200

    fixup: simple coap server and client

--- src/csc_test_server/simple_coap_server.py
+++ src/csc_test_server/simple_coap_server.py
@@ -1,13 +1,24 @@
 from aiocoap import *
+import aiocoap.resource as resource
+import asyncio
+import logging
+
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("CoAPServer")
+
 
 class BasicResource(resource.Resource):
     async def render_get(self, request):
-        return Response(payload=b"Hello, CoAP!")
+        logger.info("Received a GET request")
+        return Message(payload=b"Hello, CoAP!")
 
 def main():
     root = resource.Site()
     root.add_resource(['hello'], BasicResource())
     asyncio.Task(Context.create_server_context(root))
+    logger.info("CoAP server started")
     asyncio.get_event_loop().run_forever()
 
 if __name__ == "__main__":

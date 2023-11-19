35919c8170ed81435cdffd3ae0259d0fecfd4010
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2023-11-19 20:50:42 +0100

    fix: coap client okay

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,21 +1,32 @@
 #!/usr/bin/env bash
 
-export ARCH=iotlab-m3
-export DEFAULT_CHANNEL=23
-export ETHOS_BAUDRATE=500000
-export TAP_INTERFACE=tap7
-# this is seconds
-export JOB_WAIT_TIMEOUT=60
-export EXPERIMENT_TIME=120
+export BORDER_ROUTER_NODE=329 # border router
+export COAP_SERVER_NODE=328
+export GNRC_NETWORKING_NODE=326
+export COAP_CLIENT_NODE=327
+export SENSOR_NODE=325
 
 # https://www.iot-lab.info/legacy/tutorials/understand-ipv6-subnetting-on-the-fit-iot-lab-testbed/index.html
 export BORDER_ROUTER_IP=2001:660:5307:3107::1/64
+# export BORDER_ROUTER_IP=2001:660:5307:3108::1/64
+# export BORDER_ROUTER_IP=2001:660:5307:3109::1/64
+# export BORDER_ROUTER_IP=2001:660:5307:3110::1/64
 
-export BORDER_ROUTER_NODE=363 # border router
-export COAP_SERVER_NODE=362
-export GNRC_NETWORKING_NODE=361
-export COAP_CLIENT_NODE=360
-export SENSOR_NODE=359
+export ARCH=iotlab-m3
+
+# values are from 11-26
+export DEFAULT_CHANNEL=23
+# export DEFAULT_CHANNEL=13
+
+export ETHOS_BAUDRATE=500000
+export TAP_INTERFACE=tap7
+# export TAP_INTERFACE=tap4
+# export TAP_INTERFACE=tap5
+# export TAP_INTERFACE=tap6
+
+# this is seconds
+export JOB_WAIT_TIMEOUT=60
+export EXPERIMENT_TIME=120
 
 export BORDER_ROUTER_FOLDER_NAME=gnrc_border_router
 export BORDER_ROUTER_EXE_NAME=${BORDER_ROUTER_FOLDER_NAME}_gp12

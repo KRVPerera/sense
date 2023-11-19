e7715531b24b3007ff0b3fd986bc63d00f77d5df
KRVPerera <rukshan.viduranga@gmail.com>
2023-11-19 20:50:42 +0100

    fix: coap client okay

--- scripts/coap_test.sh
+++ scripts/coap_test.sh
@@ -2,7 +2,7 @@
 
 source ${SENSE_SCRIPTS_HOME}/setup_env.sh
 
-build_firmware ${COAP_CLIENT_HOME}
+build_wireless_firmware ${COAP_CLIENT_HOME}
 
 if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
   echo "cp ${COAP_CLIENT_HOME}/bin/${ARCH}/${COAP_CLIENT_EXE_NAME}.elf ${SENSE_FIRMWARE_HOME}"
@@ -16,15 +16,15 @@ if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
 
   wait_for_job "${coap_client_job_id}"
 
-  iotlab-experiment --jmespath="items[*].network_address | sort(@)" get --nodes
-
-  echo "aiocoap-client coap://[2001:660:3207:4c1:1711:6b10:65fd:bd36]/riot/board"
+  echo "aiocoap-client coap://[2001:660:5307:3107:a4a9:dc28:5c45:38a9]/riot/board"
   echo "coap info"
-  echo "coap get [2001:660:5307:3107:a436:ee73:926e:840c]:5683 /.well-known/core"
+  echo "coap get [2001:660:5307:3107:a4a9:dc28:5c45:38a9]:5683 /.well-known/core"
+  echo "coap get [2001:660:5307:3107:a4a9:dc28:5c45:38a9]:5683 /riot/board"
   echo "coap get 192.168.2.135:5683 /.well-known/core"
   echo "coap get example.com:5683 /.well-known/core # with sock dns"
-  echo "coap get [2001:660:5307:3107:a436:ee73:926e:840c]:5683 /temperature"
+  echo "coap get [2001:660:5307:3107:a4a9:dc28:5c45:38a9]:5683 /temperature"
 
+  echo "nc m3-${COAP_CLIENT_NODE} 20000"
   nc m3-${COAP_CLIENT_NODE} 20000
 
   stop_jobs $coap_client_job_id
--- scripts/gnrc_border_router.sh
+++ scripts/gnrc_border_router.sh
@@ -1,29 +1,25 @@
 #!/usr/bin/env bash
-#
+
+
+source ${SENSE_SCRIPTS_HOME}/setup_env.sh
+
+build_wireless_firmware ${BORDER_ROUTER_HOME}
 
 if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
-  source /opt/riot.source
-else
-  echo "[ERROR] The environment variable IOT_LAB_FRONTEND_FQDN is not set."
-fi
-
-#ARCH=nrf52840dk # not available widely in the test bed
-ARCH=iotlab-m3
-NODE=362
-
-# sensor nodes 20,21,22, 359, 361, 362
-
-make BOARD=${ARCH} -C gnrc_border_router
-echo gnrc_border_router/bin/${ARCH}/gnrc_border_router.elf
-
-if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
-  cp gnrc_border_router/bin/${ARCH}/gnrc_border_router.elf ~/shared/
+  cp ${BORDER_ROUTER_HOME}/bin/${ARCH}/${BORDER_ROUTER_EXE_NAME}.elf ${SENSE_FIRMWARE_HOME}
 
   ## submitting a job in iot test bed with the firmware it self
-  iotlab-experiment submit -n gnrc-border-router-gp12 -d 60 -l grenoble,m3,$NODE,~/shared/gnrc_border_router.elf
-  iotlab-experiment wait --timeout 30 --cancel-on-timeout
+  border_router_job_id=$(submit_border_router_job "${BORDER_ROUTER_NODE}")
+
+  create_stopper_script $border_router_job_id
+
+  wait_for_job "${border_router_job_id}"
 
   iotlab-experiment --jmespath="items[*].network_address | sort(@)" get --nodes
-  sudo ethos_uhcpd.py m3-362 tap7 2001:660:5307:3108::1/64
+
+  BORDER_ROUTER_IP=2001:660:5307:3108::1/64
+
+  create_tap_interface "${BORDER_ROUTER_NODE}"
+
   #iotlab-experiment stop
 fi

9ca03537bf00e2124eec880a2a3f0e0567fee3c2
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2023-11-23 00:34:50 +0100

    fix: build okay

--- src/network/gcoap_test/gcoap_example.h
+++ src/network/gcoap_test/gcoap_example.h
@@ -33,6 +33,35 @@
 extern "C" {
 #endif
 
+
+typedef enum resource_path {
+    TEMP,
+    TIME,
+    CORE,
+    BOARD,
+    HELLO,
+    RIOT_V,
+    SHA_256,
+    NUM_RESOURCES // Always keep this last
+} resource_path;
+
+/* server setup in rios os gcoap_server
+/.well-known/core: returns the list of available resources on the server. This is part of the CoAP specifications. 
+                    It works only with GET requests.
+/echo/: will match any request that begins with '/echo/' and will echo the remaining part of the URI path. 
+            Meant to show how the prefix works. It works only with GET requests.
+/riot/board: returns the name of the board running the server. It works only with GET requests.
+/riot/value: returns the value of an internal variable of the server. It works with GET requests 
+            and also with PUT and POST requests, which means that this value can be updated from a client.
+/riot/ver: returns the current RIOT version. Meant to show a block2 reply. It works only with GET requests.
+/sha256: creates a hash with the received payloads. It is meant to show block1 support. 
+        It returns the hash when no more blocks are pending. Only works with POST.
+
+*/
+
+
+const char* get_resource_path(resource_path path);
+
 extern uint16_t req_count;  /**< Counts requests sent by CLI. */
 
 /**
@@ -42,6 +71,10 @@ extern uint16_t req_count;  /**< Counts requests sent by CLI. */
  * @return  Exit status of the shell command
  */
 int gcoap_cli_cmd(int argc, char **argv);
+
+
+int gcoap_cli_cmd_2(resource_path path);
+void send_coap_get_request(resource_path path);
 
 /**
  * @brief   Registers the CoAP resources exposed in the example app

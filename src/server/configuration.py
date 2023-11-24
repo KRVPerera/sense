1198359fbc597361764601cb80cf8566cb2bc8f6
Ubuntu <waasnipun@gmail.com>
2023-11-24 18:20:55 +0000

    update on the server

--- src/server/configuration.py
+++ src/server/configuration.py
@@ -7,8 +7,8 @@ import os
 #HOST      = os.environ.get('INFLUXDB_HOST', '192.168.1.172')
 HOST     = os.environ.get('INFLUXDB_HOST', 'localhost')
 PORT     = os.environ.get('INFLUXDB_PORT', 8086)
-USERNAME = os.environ.get('INFLUXDB_USER', 'nipun')
-PASSWORD = os.environ.get('INFLUXDB_USER_PASSWORD', 'sense')
+USERNAME = os.environ.get('INFLUXDB_USER', 'username')
+PASSWORD = os.environ.get('INFLUXDB_USER_PASSWORD', 'password')
 DATABASE = os.environ.get('INFLUXDB_DB', 'dht')
 
 # measurements/tables

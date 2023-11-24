8561a526962e295245aab2eec587480dd4b132c2
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2023-11-24 13:47:57 +0100

    fix: read sensor

--- sensor_read_experiment.sh
+++ sensor_read_experiment.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+source setup.sh
 source ${SENSE_SCRIPTS_HOME}/setup_env.sh
 
 build_wireless_firmware ${SENSOR_READ_HOME} ${SENSOR_READ_EXE_NAME}
@@ -14,7 +15,7 @@ if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
   iotlab-profile del -n group12
   iotlab-profile addm3 -n group12 -voltage -current -power -period 8244 -avg 4
 
-  n_json=$(iotlab-experiment submit -n ${SENSOR_READ_EXE_NAME} -d 20 -l grenoble,m3,${SENSOR_NODE},${SENSE_FIRMWARE_HOME}/${SENSOR_READ_EXE_NAME}.elf,group12)
+  n_json=$(iotlab-experiment submit -n ${SENSOR_READ_EXE_NAME} -d ${EXPERIMENT_TIME} -l ${SENSE_SITE},m3,${SENSOR_NODE},${SENSE_FIRMWARE_HOME}/${SENSOR_READ_EXE_NAME}.elf,group12)
   n_node_job_id=$(echo $n_json | jq '.id')
 
   create_stopper_script $n_node_job_id

7137f2575962b6d81f82486e6602e99dd7edbf74
KRVPerera <rukshan.viduranga@gmail.com>
2023-12-01 14:48:04 +0100

    Improve documentation

--- scripts/mini_project1.sh
+++ scripts/mini_project1.sh
@@ -3,7 +3,11 @@
 source setup.sh
 source ${SENSE_SCRIPTS_HOME}/setup_env.sh
 
-build_wireless_firmware_cached ${BORDER_ROUTER_HOME} ${BORDER_ROUTER_EXE_NAME}
+if [ $ERROR_WRONG_SITE -ne 0]; then
+	exit $ERROR_WRONG_SITE
+fi
+
+buILD_WIRELess_firmware_cached ${BORDER_ROUTER_HOME} ${BORDER_ROUTER_EXE_NAME}
 build_status=$?
 if [ $build_status -ne 0 ]; then
     exit $build_status
